c42abb6acb482bea401614cfe18e78e82113a66d
Tim Schneider <mail@schneidertim.de>
2016-11-26 14:25:13 +0100

    list projects with ids

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+var gitlab = require('node-gitlab');
+
 //========================================================
 // Bot Setup
 //=========================================================
@@ -19,6 +21,14 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+//========================================================
+// GitLab API Setup
+//=========================================================
+
+var client = gitlab.createPromise({
+  api: 'https://pmgit.informatik.uni-ulm.de/api/v3',
+  privateToken: 'wZXpKxar7JhuA1HvskFA'
+});
 //=========================================================
 // Bots Dialogs
 //=========================================================
@@ -34,9 +44,25 @@ bot.dialog('/', new builder.IntentDialog()
         var note = args.matched[1].trim();
         session.send('note "%s" will be added to backlog.', note);
 
-    }).matches(/^forget it/i, function (session, args) {
+    }).matches(/^list projects/i, function (session, args) {
+        //List Gitlab Projects:
+		client.projects.list()
+		  .then(function (projects) {
+		    console.log(projects);
+		    projects.forEach(function(project) {
+    			console.log(project.path);
+    			//session.send('%s %s',project.path,project.id);
+			});
+			var str = projects.map(function(project) {return project.path + '(' + project.id+')'});
+			session.send('%s',str.join(", \r\n"));
+		    //session.send('Projects: %s', JSON.stringify(projects));
+		  })
+		  .catch(function (err) {
+		    throw err;
+		  });
+    }).matches(/^bye bye/i, function (session, args) {
         // change user's city
-        session.send('ok, i will empty the backlog');
+        session.send('Bye ;)');
         session.endDialog();
     }).onDefault(function (session) {//If none of the previous matched:
         // perform search
